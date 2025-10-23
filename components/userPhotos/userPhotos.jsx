85e51c8c5b4ab0a36f8980d20102e73b4a6cf804
Liam-M-Mays <lmays1@charlotte.edu>
2025-10-23 11:49:57 -0400

    Added Photo Component

--- components/userPhotos/userPhotos.jsx
+++ components/userPhotos/userPhotos.jsx
@@ -1,32 +1,77 @@
 import React from 'react';
-import {
-  Typography
+import { 
+  Typography, 
+  Card, 
+  CardContent, 
+  Divider 
 } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './userPhotos.css';
 
 
-/**
- * Define UserPhotos, a React componment of project #5
- */
-class UserPhotos extends React.Component {
-  constructor(props) {
-    super(props);
 
+const prettyDate = (parm) => new Date(parm).toLocaleString();
+function PhotoCard({ photo }) {
+  return (
+    <Card className="photo-card" sx={{ mb: 2 }}>
+      <img
+        className="photo-img"
+        src={`/images/${photo.file_name}`}
+        alt="user upload"
+      />
+      <CardContent>
+        <Typography variant="caption">
+          Taken: {prettyDate(photo.date_time)}
+        </Typography>
+        <Divider sx={{ my: 1 }} />
+        <div className="comments">
+          {(photo.comments || []).map((c) => (
+            <div key={c._id} className="comment-row">
+              <Typography variant="caption">{pretty(c.date_time)}</Typography>
+              {' - '}
+              <Link to={`/users/${c.user._id}`}>
+                {c.user.first_name} {c.user.last_name}
+              </Link>
+              <Typography variant="body2">{c.comment}</Typography>
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+
+
+class UserPhotos extends React.Component {
+  state = { photos: [] };
+  componentDidMount() {
+    this.loadPhotos();
   }
 
-  render() {
-    return (
-      <Typography variant="body1">
-      This should be the UserPhotos view of the PhotoShare app. Since
-      it is invoked from React Router the params from the route will be
-      in property match. So this should show details of user:
-      {this.props.match.params.userId}. You can fetch the model for the user from
-      window.models.photoOfUserModel(userId):
-        <Typography variant="caption">
-          {JSON.stringify(window.models.photoOfUserModel(this.props.match.params.userId))}
-        </Typography>
-      </Typography>
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.userId !== this.props.match.params.userId) {
+      this.loadPhotos();
+    }
+  }
 
+  loadPhotos = () => {
+    const { userId } = this.props.match.params;
+    const photos = window.models.photoOfUserModel(userId) || [];
+    this.setState({photos});
+  };
+
+  render() {
+    const {photos} = this.state;
+    return (
+      <div className="photos">
+        {photos.map((p) => (
+          <PhotoCard key={p._id} photo={p} />
+        ))}
+        {photos.length === 0 && (
+          <Typography variant="body2">No photos for this user</Typography>
+        )}
+      </div>
     );
   }
 }
